acbdc9630a666be295682fe19875667e8568388e
MMolieleng <mjmolieleng@gmail.com>
2024-03-09 00:18:22 +0200

    Added code for prompting

--- src/main/java/com/example/hellospringai/PromptController.java
+++ src/main/java/com/example/hellospringai/PromptController.java
@@ -1,13 +1,34 @@
 package com.example.hellospringai;
 
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
 
 @RestController
 public class PromptController {
 
-    @GetMapping("/prompt")
-    public String prompt() {
-        return "Hello, Spring AI!";
+    private final OllamaChatClient chatClient;
+
+    public PromptController(OllamaChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "write a simple react component") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "write a simple react component") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
     }
 }
